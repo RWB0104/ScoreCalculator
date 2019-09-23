a66df094f54407233b59517d1b537e8fd254383b
RWB0104 <psj1789@naver.com>
2019-09-23 21:43:30 +0900

    계산 무결성 검증 개선

--- ScoreCalculator/Main.Designer.cs
+++ ScoreCalculator/Main.Designer.cs
@@ -38,12 +38,18 @@
 			this.textBoxO = new System.Windows.Forms.TextBox();
 			this.textBoxX = new System.Windows.Forms.TextBox();
 			this.textBoxAvg = new System.Windows.Forms.TextBox();
+			this.labelControl1 = new System.Windows.Forms.Label();
+			this.labelControl2 = new System.Windows.Forms.Label();
+			this.buttonClear = new System.Windows.Forms.Button();
+			this.labelCopyright = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownTotal)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownCorrect)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// numericUpDownTotal
 			// 
+			this.numericUpDownTotal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+			this.numericUpDownTotal.Cursor = System.Windows.Forms.Cursors.Default;
 			this.numericUpDownTotal.Location = new System.Drawing.Point(53, 23);
 			this.numericUpDownTotal.Maximum = new decimal(new int[] {
             65535,
@@ -58,6 +64,7 @@
 			// 
 			// numericUpDownCorrect
 			// 
+			this.numericUpDownCorrect.Cursor = System.Windows.Forms.Cursors.Default;
 			this.numericUpDownCorrect.Location = new System.Drawing.Point(183, 23);
 			this.numericUpDownCorrect.Maximum = new decimal(new int[] {
             65535,
@@ -73,7 +80,7 @@
 			// labelTotal
 			// 
 			this.labelTotal.AutoSize = true;
-			this.labelTotal.Location = new System.Drawing.Point(147, 115);
+			this.labelTotal.Location = new System.Drawing.Point(147, 83);
 			this.labelTotal.Name = "labelTotal";
 			this.labelTotal.Size = new System.Drawing.Size(29, 12);
 			this.labelTotal.TabIndex = 2;
@@ -82,7 +89,7 @@
 			// labelO
 			// 
 			this.labelO.AutoSize = true;
-			this.labelO.Location = new System.Drawing.Point(17, 115);
+			this.labelO.Location = new System.Drawing.Point(17, 83);
 			this.labelO.Name = "labelO";
 			this.labelO.Size = new System.Drawing.Size(29, 12);
 			this.labelO.TabIndex = 3;
@@ -91,7 +98,7 @@
 			// labelX
 			// 
 			this.labelX.AutoSize = true;
-			this.labelX.Location = new System.Drawing.Point(17, 142);
+			this.labelX.Location = new System.Drawing.Point(17, 110);
 			this.labelX.Name = "labelX";
 			this.labelX.Size = new System.Drawing.Size(29, 12);
 			this.labelX.TabIndex = 4;
@@ -100,7 +107,7 @@
 			// labelAvg
 			// 
 			this.labelAvg.AutoSize = true;
-			this.labelAvg.Location = new System.Drawing.Point(147, 142);
+			this.labelAvg.Location = new System.Drawing.Point(147, 110);
 			this.labelAvg.Name = "labelAvg";
 			this.labelAvg.Size = new System.Drawing.Size(29, 12);
 			this.labelAvg.TabIndex = 5;
@@ -108,7 +115,7 @@
 			// 
 			// textBoxTotal
 			// 
-			this.textBoxTotal.Location = new System.Drawing.Point(183, 112);
+			this.textBoxTotal.Location = new System.Drawing.Point(183, 80);
 			this.textBoxTotal.Name = "textBoxTotal";
 			this.textBoxTotal.ReadOnly = true;
 			this.textBoxTotal.Size = new System.Drawing.Size(72, 21);
@@ -116,7 +123,7 @@
 			// 
 			// textBoxO
 			// 
-			this.textBoxO.Location = new System.Drawing.Point(53, 112);
+			this.textBoxO.Location = new System.Drawing.Point(53, 80);
 			this.textBoxO.Name = "textBoxO";
 			this.textBoxO.ReadOnly = true;
 			this.textBoxO.Size = new System.Drawing.Size(72, 21);
@@ -124,7 +131,7 @@
 			// 
 			// textBoxX
 			// 
-			this.textBoxX.Location = new System.Drawing.Point(53, 139);
+			this.textBoxX.Location = new System.Drawing.Point(53, 107);
 			this.textBoxX.Name = "textBoxX";
 			this.textBoxX.ReadOnly = true;
 			this.textBoxX.Size = new System.Drawing.Size(72, 21);
@@ -132,17 +139,59 @@
 			// 
 			// textBoxAvg
 			// 
-			this.textBoxAvg.Location = new System.Drawing.Point(183, 139);
+			this.textBoxAvg.Location = new System.Drawing.Point(183, 107);
 			this.textBoxAvg.Name = "textBoxAvg";
 			this.textBoxAvg.ReadOnly = true;
 			this.textBoxAvg.Size = new System.Drawing.Size(72, 21);
 			this.textBoxAvg.TabIndex = 9;
 			// 
+			// labelControl1
+			// 
+			this.labelControl1.AutoSize = true;
+			this.labelControl1.Location = new System.Drawing.Point(17, 25);
+			this.labelControl1.Name = "labelControl1";
+			this.labelControl1.Size = new System.Drawing.Size(29, 12);
+			this.labelControl1.TabIndex = 10;
+			this.labelControl1.Text = "총합";
+			// 
+			// labelControl2
+			// 
+			this.labelControl2.AutoSize = true;
+			this.labelControl2.Location = new System.Drawing.Point(147, 25);
+			this.labelControl2.Name = "labelControl2";
+			this.labelControl2.Size = new System.Drawing.Size(29, 12);
+			this.labelControl2.TabIndex = 11;
+			this.labelControl2.Text = "정답";
+			// 
+			// buttonClear
+			// 
+			this.buttonClear.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.buttonClear.Location = new System.Drawing.Point(101, 151);
+			this.buttonClear.Name = "buttonClear";
+			this.buttonClear.Size = new System.Drawing.Size(75, 23);
+			this.buttonClear.TabIndex = 12;
+			this.buttonClear.Text = "Clear";
+			this.buttonClear.UseVisualStyleBackColor = true;
+			this.buttonClear.Click += new System.EventHandler(this.ButtonClear_Click);
+			// 
+			// labelCopyright
+			// 
+			this.labelCopyright.AutoSize = true;
+			this.labelCopyright.Location = new System.Drawing.Point(12, 186);
+			this.labelCopyright.Name = "labelCopyright";
+			this.labelCopyright.Size = new System.Drawing.Size(259, 12);
+			this.labelCopyright.TabIndex = 13;
+			this.labelCopyright.Text = "Copyright (C) 2019. RWB. All Rights Reserve.";
+			// 
 			// Main
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(279, 187);
+			this.ClientSize = new System.Drawing.Size(279, 206);
+			this.Controls.Add(this.labelCopyright);
+			this.Controls.Add(this.buttonClear);
+			this.Controls.Add(this.labelControl2);
+			this.Controls.Add(this.labelControl1);
 			this.Controls.Add(this.textBoxAvg);
 			this.Controls.Add(this.textBoxX);
 			this.Controls.Add(this.textBoxO);
@@ -174,6 +223,10 @@
 		private System.Windows.Forms.TextBox textBoxO;
 		private System.Windows.Forms.TextBox textBoxX;
 		private System.Windows.Forms.TextBox textBoxAvg;
+		private System.Windows.Forms.Label labelControl1;
+		private System.Windows.Forms.Label labelControl2;
+		private System.Windows.Forms.Button buttonClear;
+		private System.Windows.Forms.Label labelCopyright;
 	}
 }
 
--- ScoreCalculator/Main.cs
+++ ScoreCalculator/Main.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// 정보처리기사 채점용 계산기
 namespace ScoreCalculator
 {
+    // Main Form Class
 	public partial class Main : Form
 	{
 		private int totalNum = 0;
@@ -17,11 +12,13 @@ namespace ScoreCalculator
 		private int X = 0;
 		private double average = 0;
 
+        // Form Initialize
 		public Main()
 		{
 			InitializeComponent();
 		}
 
+        // 총합 갯수 증감
 		private void NumericUpDownTotal_ValueChanged(object sender, EventArgs e)
 		{
 			totalNum = Convert.ToInt32(numericUpDownTotal.Value);
@@ -30,6 +27,7 @@ namespace ScoreCalculator
 			Calculate();
 		}
 
+        // 정답 갯수 증감
 		private void NumericUpDownCorrect_ValueChanged(object sender, EventArgs e)
 		{
 			correctNum = Convert.ToInt32(numericUpDownCorrect.Value);
@@ -38,34 +36,55 @@ namespace ScoreCalculator
 			Calculate();
 		}
 
+        // 총합 갯수 입력
 		private void NumericUpDownTotal_KeyUp(object sender, KeyEventArgs e)
 		{
+            // 총합 갯수 증감 처리
 			NumericUpDownTotal_ValueChanged(sender, e);
 		}
 
+        // 정답 갯수 입력
 		private void NumericUpDownCorrect_KeyUp(object sender, KeyEventArgs e)
 		{
+            // 정답 갯수 증감 처리
 			NumericUpDownCorrect_ValueChanged(sender, e);
 		}
 
+		// 초기화 버튼
+		private void ButtonClear_Click(object sender, EventArgs e)
+		{
+			numericUpDownTotal.Value = 0;
+			numericUpDownCorrect.Value = 0;
+		}
+
+		// 점수 계산
 		private void Calculate()
 		{
+            // 총합, 정답이 전부 0일 경우
 			if (totalNum == 0 && correctNum == 0)
 			{
-				// TODO
+                textBoxTotal.Text = "";
+                textBoxO.Text = "";
+                textBoxX.Text = "";
+                textBoxAvg.Text = "";
 			}
 
+            // 총합만 0일 경우
 			else if (totalNum == 0 && correctNum != 0)
 			{
-				// TODO
+				numericUpDownCorrect.Value = 0;
+				numericUpDownTotal.Focus();
 			}
 
+            // 총합보다 정답이 클 경우
 			else if (totalNum < correctNum)
 			{
+                // 정답의 갯수를 총합으로 제한
 				numericUpDownCorrect.Value = totalNum;
 				textBoxO.Text = totalNum.ToString();
 			}
 
+            // 모든 검증에 부합할 경우
 			else
 			{
 				X = totalNum - correctNum;
